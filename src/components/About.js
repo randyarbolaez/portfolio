6c89678c852ecfe42297b4965495a192de2ba346
randyarbolaez <randyarbolaez@outlook.com>
2021-10-05 09:17:48 -0400

    restyled about section

--- src/components/About.js
+++ src/components/About.js
@@ -4,42 +4,48 @@ import "../styles/About.css";
 import {
   AiOutlineLinkedin,
   AiOutlineMail,
-  AiOutlineGithub
+  AiOutlineGithub,
 } from "react-icons/ai";
 
 const About = (props) => (
   <div className="container-about">
-  {!props.footer && <h1 className="title-about">Randy Arbolaez</h1>}
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <a
-        href="https://www.linkedin.com/in/randyarbolaez/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h1>
-          <AiOutlineLinkedin />
-        </h1>
-      </a>
-      <a
-        href="mailto:randyarbolaez@outlook.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h1>
-          <AiOutlineMail />
-        </h1>
-      </a>
-      <a
-        href="https://github.com/randyarbolaez"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h1>
-          <AiOutlineGithub />
-        </h1>
-      </a>
-    </div>
-  {props.footer && <h1 style={{padding:"0", marginTop:"0"}} className="title-about">Randy Arbolaez</h1>}
+    {!props.footer && <h1 className="title-about">Randy Arbolaez</h1>}
+    {props.footer && (
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <a
+          href="https://www.linkedin.com/in/randyarbolaez/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h1>
+            <AiOutlineLinkedin />
+          </h1>
+        </a>
+        <a
+          href="mailto:randyarbolaez@outlook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h1>
+            <AiOutlineMail />
+          </h1>
+        </a>
+        <a
+          href="https://github.com/randyarbolaez"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h1>
+            <AiOutlineGithub />
+          </h1>
+        </a>
+      </div>
+    )}
+    {props.footer && (
+      <h1 style={{ padding: "0", marginTop: "0" }} className="title-about">
+        Randy Arbolaez
+      </h1>
+    )}
   </div>
 );
 
